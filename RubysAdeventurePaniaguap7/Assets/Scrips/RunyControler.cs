8ac72d1488625469d532944e4b0df4349ecbd28c
723103 <142541441+723103@users.noreply.github.com>
2023-10-31 15:24:20 -0700

    h
    hfg

--- RubysAdeventurePaniaguap7/Assets/Scrips/RunyControler.cs
+++ RubysAdeventurePaniaguap7/Assets/Scrips/RunyControler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +9,20 @@ public class RunyControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("horizontal"); 
-        Debug.Log(horizontal);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
         transform.position = position;
     }
 }
